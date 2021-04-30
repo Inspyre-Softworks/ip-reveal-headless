1d5870f53b50f0c96b3251cc90acbf9b03c31dc9
Taylor B <tayjaybabee@gmail.com>
2021-04-30 09:23:44 -0500

    Update audio library, version bump 1.0 -> 1.
    Files changed:
    
    * pyproject.toml:
      * Version bump: v1.0 -> v1.1
      * Removed `simpleaudio` as a dependency
      * Added [playsound](https://pypi.org/project/playsound/)
    
    * requirements.txt:
      * Removed `simpleaudio`
    
    * ip_reveal/assets/sounds:
      * Removed `simpleaudio` calls and replaced with working `playsound` calls
    
    * ip_reveal/assets/sounds/alerts:
      * Removed `simpleaudio` calls and replaced with working `playsound` calls

--- ip_reveal/assets/sounds/__init__.py
+++ ip_reveal/assets/sounds/__init__.py
@@ -1,5 +1,5 @@
 from ip_reveal.assets.sounds import alerts
-import simpleaudio as sa
+from playsound import playsound
 
 
 class Alerts(object):
@@ -9,7 +9,6 @@ class Alerts(object):
         """
         self.asset_fp = alerts.ALERT_AUDIO_FP
         self.o_pulse_fp = alerts.O_PULSE_FP
-        self.o_pulse_alert = sa.WaveObject.from_wave_file(self.o_pulse_fp)
-
+        
     def play(self):
-        self.o_pulse_alert.play()
+        playsound(self.o_pulse_fp)
