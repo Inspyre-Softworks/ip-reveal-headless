49c8e8e83d0345cdb747318cd5c0f38279cccada
Taylor B <tayjaybabee@gmail.com>
2021-07-13 20:27:16 -0500

    Changes for next version

---
+++ ip_reveal/gui/ windows/preferences.py
@@ -0,0 +1,14 @@
+import PySimpleGUIQt as psg
+
+from ip_reveal.config import CONF
+
+
+class Window(object):
+    
+    active = False
+    
+    layout = [
+            [
+                    
+                    ]
+            ]
